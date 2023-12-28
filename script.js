7fdcf5b958fbe6162bc12dddfb4bd874be2be7b0
Sombode <25levyle@gmail.com>
2023-12-28 16:08:24 -0500

    Formatted/fixed homepage

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const largeGear = document.getElementById("largeGear");
 const smallGear = document.getElementById("smallGear");
 const gearHandle = document.getElementById("handle");
+const gearHandleInteract = document.getElementById("handleInteract");
 
-gearHandle.onmousedown = function(event) {
+gearHandleInteract.onmousedown = function(event) {
   function gearTurn(event) {
     const rect = smallGear.getBoundingClientRect();
     const angle = Math.atan2(event.clientY - (rect.y + rect.height / 2), event.clientX - (rect.x + rect.width / 2));
@@ -11,13 +12,13 @@ gearHandle.onmousedown = function(event) {
   function releaseHandle() {
     document.removeEventListener("mousemove", gearTurn);
     document.removeEventListener("mouseup", releaseHandle);
-    gearHandle.style.filter = "";
+    gearHandleInteract.style.filter = "";
   }
   const rect = smallGear.getBoundingClientRect();
   const angle = Math.atan2(event.clientY - (rect.y + rect.height / 2), event.clientX - (rect.x + rect.width / 2));
   const grabOffset = parseFloat(getComputedStyle(gearHandle).getPropertyValue("--rotation")) - angle;
   gearTurn(event);
-  gearHandle.style.filter = "brightness(80%)";
+  gearHandleInteract.style.filter = "brightness(80%)";
   document.addEventListener("mousemove", gearTurn);
   document.addEventListener("mouseup", releaseHandle);
 }
